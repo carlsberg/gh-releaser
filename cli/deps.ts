991554817b3e1f71969b76d909f203af9f688e3b
João Cerqueira <oss@cerqueira.io>
2021-09-13 23:02:06 +0100

    feat: added start release command

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,2 +1,10 @@
+// x/gh-releaser
+export {
+  createBranch,
+  createRelease,
+  getBranch,
+  openPullRequest,
+} from "../lib/mod.ts";
+
 // x/yargs
 export { default as yargs } from "https://deno.land/x/yargs@v17.1.1-deno/deno.ts";
--- cli/gh-releaser.ts
+++ cli/gh-releaser.ts
@@ -1,8 +1,37 @@
 import { yargs } from "./deps.ts";
 import { version } from "../version.ts";
+import { startCommand } from "./cmd/start.ts";
+
+export const DEFAULT_MAIN_BRANCH = "main";
+export const DEFAULT_DEVELOP_BRANCH = "develop";
 
 yargs()
   .scriptName("gh-releaser")
   .alias("h", "help")
+  .command(
+    "start <tag>",
+    "starts a new release",
+    (y: ReturnType<typeof yargs>) => {
+      y.positional("tag", {
+        type: "string",
+        required: true,
+        describe: "Tag to be released",
+      });
+
+      y.option("main-branch", {
+        type: "string",
+        describe: "Repository's main branch",
+        default: DEFAULT_MAIN_BRANCH,
+      });
+
+      y.option("develop-branch", {
+        type: "string",
+        describe: "Repository's develop branch",
+        default: DEFAULT_DEVELOP_BRANCH,
+      });
+    },
+    startCommand,
+  )
+  .demandCommand(1)
   .version(version)
   .parse(Deno.args);
