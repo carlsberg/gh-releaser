3ba0a40f4e9a7333d6ea39c2ec43354eb1a6c3e6
João Cerqueira <oss@cerqueira.io>
2021-11-15 11:20:09 +0000

    init commit

--- scripts/bump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RELEASE_BRANCH=release/$(git semver next)
-
-git checkout -b $RELEASE_BRANCH
-git semver bump -f version.ts:version -f gh-releaser:tag
-git push -u origin $RELEASE_BRANCH
--- scripts/release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir bin
-deno compile -A --output bin/gh-releaser --target x86_64-unknown-linux-gnu cli/gh-releaser.ts
-gh release create $(git semver latest) bin/gh-releaser
-rm -rf bin
